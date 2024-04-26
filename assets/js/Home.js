3b2c2093109d4341996c3fe7e4a354629e13dc28
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-04-26 21:21:07 +0200

    Update

--- assets/js/Home.js
+++ assets/js/Home.js
@@ -1,5 +1,5 @@
 // Définition de la fonction pour la redirection vers la page LoginPage.html
 function redirectToLoginPage() {
     // Redirection vers la page LoginPage.html
-    window.location.href = "LoginPage.html";
+    window.location.href = "templates/LoginPage.html";
 }
