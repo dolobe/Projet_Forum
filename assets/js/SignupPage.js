c9ae597046c69a4482372f71ec2d144811361b85
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-04-26 21:20:46 +0200

    Add func SignupPage

---
+++ assets/js/SignupPage.js
@@ -0,0 +1,5 @@
+// Définition de la fonction pour la redirection vers la page LoginPage.html
+function redirectToSignupPage() {
+    // Redirection vers la page LoginPage.html
+    window.location.href = "templates/SignupPage.html";
+}
