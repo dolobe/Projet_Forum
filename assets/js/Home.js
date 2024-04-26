13c56c78822734ba8699ceb570f58344a7c99b53
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-04-26 15:16:00 +0200

    Update Home.css

---
+++ assets/js/Home.js
@@ -0,0 +1,5 @@
+// Définition de la fonction pour la redirection vers la page LoginPage.html
+function redirectToLoginPage() {
+    // Redirection vers la page LoginPage.html
+    window.location.href = "LoginPage.html";
+}
