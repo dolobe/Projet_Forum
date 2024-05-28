dda48daa6ca269b9381a68a23d3c38c0f8b02ce8
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-05-28 09:54:33 +0200

    add a func getUsernameByEmail

--- src/handlers/Category.go
+++ src/handlers/Category.go
@@ -1,22 +1,55 @@
 package handlers
 
 import (
-	"io/ioutil"
+	"html/template"
 	"net/http"
 )
 
 func HandleCategoryPage(w http.ResponseWriter, r *http.Request) {
-	htmlFile, err := ioutil.ReadFile("templates/Category.html")
+	email, err := GetSessionEmail(r)
+	if err != nil {
+		http.Redirect(w, r, "/Login", http.StatusSeeOther)
+		return
+	}
+
+	username, err := getUsernameByEmail(email)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/html")
-
-	_, err = w.Write(htmlFile)
+	tmpl, err := template.ParseFiles("templates/category.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	donnees := struct {
+		Username string
+	}{
+		Username: username,
+	}
+
+	err = tmpl.Execute(w, donnees)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getUsernameByEmail(email string) (string, error) {
+	basededonnees, err := DatabasePath()
+	if err != nil {
+		return "", err
+	}
+	defer basededonnees.Close()
+
+	var username string
+	query := `SELECT pseudo FROM Users WHERE email = ?`
+	err = basededonnees.QueryRow(query, email).Scan(&username)
+	if err != nil {
+		return "", err
+	}
+
+	return username, nil
 }
