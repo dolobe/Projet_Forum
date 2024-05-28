ce07acc65b093e6921744cd37c99ecbe0aca2e3b
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-05-28 14:11:14 +0200

    delete Home.js..

--- assets/js/Home.js
+++
@@ -1,5 +0,0 @@
-// Définition de la fonction pour la redirection vers la page LoginPage.html
-function redirectToLoginPage() {
-    // Redirection vers la page LoginPage.html
-    window.location.href = "templates/LoginPage.html";
-}
