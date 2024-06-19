360f3f3ee51d8f49ca8ae09df4471dc775895b0e
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-06-19 19:39:28 +0200

    add getCategories

--- src/handlers/Table.go
+++ src/handlers/Table.go
@@ -2,53 +2,92 @@ package handlers
 
 import (
 	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type PageData struct {
 	Categories []Category
+	Username   string
 }
 
+type User struct {
+	ID       string
+	Name     string
+	LastName string
+	Pseudo   string
+	Email    string
+	Password string
+}
+
+// Category is a struct that represents a category in the database
 func DatabasePath() (*sql.DB, error) {
 	basededonnees, err := sql.Open("sqlite3", "./src/DataBase/forum.db")
 	if err != nil {
 		return nil, err
 	}
-	if err := createTableUsers(basededonnees); err != nil {
+	if err := createTables(basededonnees); err != nil {
 		return nil, err
 	}
 	return basededonnees, nil
 }
 
-func createTableUsers(basededonnees *sql.DB) error {
+// CreateTables User
+func createTables(db *sql.DB) error {
 	createTableUsersQuery := `CREATE TABLE IF NOT EXISTS Users (
-		id TEXT PRIMARY KEY,
-		name TEXT UNIQUE,
-		last_name TEXT UNIQUE,
-		pseudo TEXT UNIQUE,
-		email TEXT UNIQUE,
-		password TEXT
-	)`
-	_, err := basededonnees.Exec(createTableUsersQuery)
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE,
+        last_name TEXT UNIQUE,
+        pseudo TEXT UNIQUE,
+        email TEXT UNIQUE,
+        password TEXT
+    )`
+
+	// CreateTable Category
+	createTableCategoryQuery := `CREATE TABLE IF NOT EXISTS Category (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        CategoryName TEXT UNIQUE,
+        SubjectName TEXT
+    )`
+
+	_, err := db.Exec(createTableUsersQuery)
 	if err != nil {
 		return err
 	}
 
-	createTableCategoryQuery := `CREATE TABLE IF NOT EXISTS Category (
-		id TEXT PRIMARY KEY,
-		CategoryName TEXT UNIQUE,
-		SubjectName TEXT
-	)`
-	_, err = basededonnees.Exec(createTableCategoryQuery)
+	_, err = db.Exec(createTableCategoryQuery)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
+// Get Gategories
 func getCategories(basededonnees *sql.DB) ([]Category, error) {
 	rows, err := basededonnees.Query(`SELECT CategoryName, SubjectName FROM Category`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var categories []Category
+	for rows.Next() {
+		var category Category
+		if err := rows.Scan(&category.CategoryName, &category.SubjectName); err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return categories, nil
+}
+
+// GetSubjects
+func getSubjects(db *sql.DB) ([]Category, error) {
+	rows, err := db.Query(`SELECT CategoryName, SubjectName FROM Category`)
 	if err != nil {
 		return nil, err
 	}
