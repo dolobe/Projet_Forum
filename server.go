a2abf8ea1582ae294270be552540111322b5250d
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-06-19 22:45:32 +0200

    Udate

--- server.go
+++ server.go
@@ -7,19 +7,28 @@ import (
 )
 
 func main() {
+	// Handle the assets
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
+	// Handle the pages
 	http.HandleFunc("/", handlers.HandleHomePage)
 	http.HandleFunc("/Login", handlers.HandleLoginPage)
 	http.HandleFunc("/signup", handlers.HandleSignupPage)
 	http.HandleFunc("/category", handlers.HandleCategoryPage)
 	http.HandleFunc("/allCategory", handlers.HandleAllCategoryPage)
+
 	http.HandleFunc("/post", handlers.HandlePostPage)
 	http.HandleFunc("/googleSignup", handlers.HandleGoogleSignup)
 	http.HandleFunc("/googleLogin", handlers.HandleGoogleLogin)
 	http.HandleFunc("/google/callback", handlers.HandleGoogleCallback)
 	http.HandleFunc("/pseudo", handlers.HandlePseudo)
 
+	http.HandleFunc("/postComment", handlers.HandlePostComment)
+	http.HandleFunc("/postReply", handlers.HandlePostReply)
+
+	http.HandleFunc("/getComments", handlers.HandleGetComments)
+
+	// Start the serverS
 	fmt.Println("Démarrage du serveur sur le port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
